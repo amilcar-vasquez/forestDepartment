d388f1c31d9634459ed7894f6e9c03fedd1ac949
Israel Vasquez <rasiel@gmail.com>
2024-06-07 13:03:31 -0600

    add the admin for CITESList model

--- licensing/admin.py
+++ licensing/admin.py
@@ -8,8 +8,13 @@ class ApplicationAdmin(admin.ModelAdmin):
 
     search_fields = ('importer_name', 'type', 'goods', 'importer_address', 'importer_city', 'importer_state', 'importer_phone', 'importer_email', 'exporter_name', 'exporter_address', 'exporter_city', 'exporter_state', 'mode_of_transport', 'port_of_entry', 'port_of_exit', 'treatment', 'date_received', 'date_approved', 'date_expires', 'packaging_list_approved', 'approval')
 
+class CITESListAdmin(admin.ModelAdmin):
+    list_display = ('CITES_id', 'species', 'sub_species', 'scientific_name', 'rank', 'listing')
+
+    search_fields = ('species', 'scientific_name')
+
 admin.site.register(Application, ApplicationAdmin)
 admin.site.register(Lumber)
 admin.site.register(Species)
 admin.site.register(Profile)
-admin.site.register(CITESList)
+admin.site.register(CITESList, CITESListAdmin)
